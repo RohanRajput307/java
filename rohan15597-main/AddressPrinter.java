3e9c7c831b6f3add785d36913990c5d299f73c46
RohanRajput307 <rohan.rajput8999@gmail.com>
2025-08-17 10:18:33 -0700

    Delete rohan15597-main directory

--- rohan15597-main/AddressPrinter.java
+++
@@ -1,9 +0,0 @@
-public class AddressPrinter {
-    public static void main(String[] args) {
-        System.out.println("Aryan Paul");
-        System.out.println("WAVE CITY");
-        System.out.println("Ghaziabad-201002");
-        System.out.println("Uttar Pradesh");
-        
-    }
-}
